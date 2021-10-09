390d81e2ecd4ceedaf208d68531a7321a44d3d27
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-10-09 10:30:24 -0700

    ADD test - adoption

---
+++ test/TestAdoption.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import 'truffle/Assert.sol';
+import 'truffle/DeployedAddresses.sol';
+import '../contracts/Adoption.sol';
+
+contract TestAdoption {
+  // address of adoption contract subject to testing
+  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+  // id of the pet that will be tested
+  uint expectedPetId = 8;
+
+  // expected owner of the adopted pet
+  address expectedAdopter = address(this);
+
+  function testUserCanAdopt() public {
+    uint returnedId = adoption.adopt(expectedPetId);
+    Assert.equal(returnedId, expectedPetId, 'identifiers do not match');
+  }
+}
