c6bebd85db9d6808e4db3c7ba4c0571cb1867930
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-10-09 10:16:17 -0700

    UPDATE ch3 - adopters

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.5.0;
 contract Adoption {
   address[16] adopters;
 
-  function adopt(uint petId) public returns(uint) {
+  function adopt(uint petId)
+    public
+    returns(uint)
+  {
     require(petId >= 0 && petId < 16); // boundary check/requirement
     adopters[petId] = msg.sender;
     return petId;
+  }
+
+  function getAdopters()
+    public
+    view
+    returns(address[16] memory)
+  {
+    return adopters;
   }
 }
